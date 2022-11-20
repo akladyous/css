85d6b02185e1d3a3018fb58de7934de04bb11fcc
Boula Akladyous <akladyous@gmail.com>
2022-11-20 14:57:56 -0500

    tailwind navbar

--- tailwind/tailwind-navbar.js
+++ tailwind/tailwind-navbar.js
@@ -1,8 +1,9 @@
 // Burger menus
 document.addEventListener("DOMContentLoaded", function () {
     // open
-    const burger = document.querySelectorAll(".navbar-burger");
-    const menu = document.querySelectorAll(".navbar-menu");
+
+    var burger = document.querySelectorAll(".navbar-burger");
+    var menu = document.querySelectorAll(".navbar-menu");
 
     if (burger.length && menu.length) {
         for (var i = 0; i < burger.length; i++) {
@@ -15,8 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // close
-    const close = document.querySelectorAll(".navbar-close");
-    const backdrop = document.querySelectorAll(".navbar-backdrop");
+    var close = document.querySelectorAll(".navbar-close");
+    var backdrop = document.querySelectorAll(".navbar-backdrop");
 
     if (close.length) {
         for (var i = 0; i < close.length; i++) {
