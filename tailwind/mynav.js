dd4ecb114742b86487a75c0e2a591fa458c3954b
Boula Akladyous <akladyous@gmail.com>
2023-07-04 11:19:12 -0400

    Untracked files on css

--- tailwind/mynav.js
+++
@@ -1,5 +0,0 @@
-const menuButton = document.querySelector("#menu-btn");
-const menuList = document.querySelector("#menu-list");
-menuButton.addEventListener("click", () => {
-    menuList.classList.toggle("hidden");
-});
